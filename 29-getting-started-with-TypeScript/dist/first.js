cca35bcbfb16accae7e81c79862144ee59b56bb1
nomanme <abdullah.noman083@gmail.com>
2021-08-27 01:01:18 +0600

    ts to js

--- 29-getting-started-with-TypeScript/dist/first.js
+++ 29-getting-started-with-TypeScript/dist/first.js
@@ -3,3 +3,27 @@
 let myMoney = 500;
 myMoney = 100;
 myMoney = 0;
+// 29-4 Basic typescript type number string boolean
+let age = 24;
+let club = 'Real Madrid';
+// const isFamous: boolean; // error
+let famous;
+console.log(club);
+// 29-5 Set function parameter type and function return type
+function add(num1, num2) {
+    return num1 + num2;
+}
+add(2, 3);
+add('Mohammad', 'Noman');
+add(24, 'Noman');
+function fullName(firstName, lastName) {
+    return firstName + ' ' + lastName;
+}
+const user = fullName('Abdul', 'Khan');
+// 29-6 function void return and arrow function type
+function doubleIt(num) {
+    const result = num * 2;
+    console.log(result);
+}
+const output = doubleIt(10);
+console.log('output', output);
