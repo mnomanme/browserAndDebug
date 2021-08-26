b112320a0c53ecbbdb719e5b2de76384d19b535d
nomanme <abdullah.noman083@gmail.com>
2021-08-26 23:06:46 +0600

    basic typescript

--- 29-getting-started-with-TypeScript/first.ts
+++ 29-getting-started-with-TypeScript/first.ts
@@ -2,3 +2,11 @@
 let myMoney = 500;
 myMoney = 100;
 myMoney = 0;
+
+// 29-4 Basic typescript type number string boolean
+let age: number = 24;
+let club: string = 'Real Madrid';
+// const isFamous: boolean; // error
+let famous: boolean;
+
+console.log(club);
