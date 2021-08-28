1e6e17f89e514ff940a3d590e2736aeb2601acee
nomanme <abdullah.noman083@gmail.com>
2021-08-28 07:04:18 +0600

    show image & enter key

--- 30-assignment/js/app.js
+++ 30-assignment/js/app.js
@@ -4,13 +4,25 @@ const galleryHeader = document.querySelector('.gallery-header');
 const searchBtn = document.getElementById('search-btn');
 const sliderBtn = document.getElementById('create-slider');
 const sliderContainer = document.getElementById('sliders');
-// selected image
-let sliders = [];
 
-// If this key doesn't work
-// Find the name in the url and go to their website
-// to create your own api key
-const KEY = '15674931-a9d714b6e9d654524df198e00&q';
+// my API KEY
+// const API = `// https://pixabay.com/api/?key=${KEY}&q=${query}&image_type=photo&pretty=true`;
+const KEY = '23123828-5dab4854dbd652a450144e361';
+
+// to get data
+const getImages = (query) => {
+	fetch(`https://pixabay.com/api/?key=${KEY}&q=${query}&image_type=photo&pretty=true`)
+		.then((response) => response.json())
+		.then((data) => showImages(data.hits))
+		.catch((err) => console.log(err));
+};
+
+// Enter Key for search
+search.addEventListener('keypress', function (event) {
+	if (event.key === 'Enter') {
+		searchBtn.click();
+	}
+});
 
 // show images
 const showImages = (images) => {
@@ -21,18 +33,16 @@ const showImages = (images) => {
 	images.forEach((image) => {
 		let div = document.createElement('div');
 		div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
-		div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
+		div.innerHTML = ` 
+		<img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") 
+		src="${image.webformatURL}" alt="${image.tags}">
+		`;
 		gallery.appendChild(div);
 	});
 };
 
-const getImages = (query) => {
-	fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
-		.then((response) => response.json())
-		.then((data) => showImages(data.hitS))
-		.catch((err) => console.log(err));
-};
-
+// image selected & count image
+let sliders = [];
 let slideIndex = 0;
 const selectItem = (event, img) => {
 	let element = event.target;
@@ -42,9 +52,11 @@ const selectItem = (event, img) => {
 	if (item === -1) {
 		sliders.push(img);
 	} else {
-		alert('Hey, Already added !');
+		sliders.splice(item, 1);
+		// alert('Hey, Already added !');
 	}
 };
+
 var timer;
 const createSlider = () => {
 	// check slider image length
@@ -52,26 +64,25 @@ const createSlider = () => {
 		alert('Select at least 2 image.');
 		return;
 	}
-	// crate slider previous next area
+	// create slider previous next area
 	sliderContainer.innerHTML = '';
 	const prevNext = document.createElement('div');
 	prevNext.className = 'prev-next d-flex w-100 justify-content-between align-items-center';
 	prevNext.innerHTML = ` 
-  <span class="prev" onclick="changeItem(-1)"><i class="fas fa-chevron-left"></i></span>
-  <span class="next" onclick="changeItem(1)"><i class="fas fa-chevron-right"></i></span>
-  `;
+        <span class="prev" onclick="changeItem(-1)"><i class="fas fa-chevron-left"></i></span>
+        <span class="next" onclick="changeItem(1)"><i class="fas fa-chevron-right"></i></span>
+        `;
 
 	sliderContainer.appendChild(prevNext);
 	document.querySelector('.main').style.display = 'block';
-	// hide image aria
+	// hide image area
 	imagesArea.style.display = 'none';
 	const duration = document.getElementById('duration').value || 1000;
 	sliders.forEach((slide) => {
 		let item = document.createElement('div');
 		item.className = 'slider-item';
-		item.innerHTML = `<img class="w-100"
-    src="${slide}"
-    alt="">`;
+		item.innerHTML = `
+            <img class="w-100" src="${slide}" alt="">`;
 		sliderContainer.appendChild(item);
 	});
 	changeSlide(0);
