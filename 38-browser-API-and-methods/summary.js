86a26961f4de862b59083234983ceb7a8bde6b24
nomanme <abdullah.noman083@gmail.com>
2021-09-05 01:43:30 +0600

    all recap

---
+++ 38-browser-API-and-methods/summary.js
@@ -0,0 +1,28 @@
+// 38-9 Module Summary, ContentEditable, chrome restart
+
+alert('Check');
+
+confirm('Are you?');
+
+prompt('Your name');
+
+location.reload();
+location.href = '';
+location.hash = '';
+
+// `http://127.0.0.1:5500/38-browser-API-and-methods/shop.html`
+
+history.back();
+history.go();
+
+// cookies
+
+// sessionStorage vs localStorage
+
+// cokies vs sessionStorage vs localStorgae
+
+// web API vs browser API
+
+document.body.contentEditable = true;
+
+// clipboard
