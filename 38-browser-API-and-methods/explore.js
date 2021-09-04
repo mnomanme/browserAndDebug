8327eec7e006fb54d947342955597c465dda8f54
nomanme <abdullah.noman083@gmail.com>
2021-09-04 23:58:06 +0600

    BOM vs DOM

---
+++ 38-browser-API-and-methods/explore.js
@@ -0,0 +1,16 @@
+// 38-1 Module Overview, BOM vs DOM, Browser API
+
+const name = 15;
+const student = {
+	name: 'Tonmoy',
+	friend: 'Mahmudul',
+	job: 'Business',
+	makeMovie: function (name) {
+		console.log(name);
+	},
+};
+
+student.makeMovie('Game of Thrones');
+
+// document.getElementById('name');
+fetch(``);
