c570c6656383e113416822bc497f37d03929b9f3
nomanme <abdullah.noman083@gmail.com>
2021-09-05 00:35:14 +0600

    location

---
+++ 38-browser-API-and-methods/location.js
@@ -0,0 +1,9 @@
+// 38-3 Location, URL parts, query string, href, hash, assign, reload
+
+const refreshPage = () => {
+	location.reload();
+};
+
+const visit = () => {
+	location.assign('https://quran.com/');
+};
