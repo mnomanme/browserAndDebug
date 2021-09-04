87f44bc04c67a5709452f9b66d190ec0cafbfdbc
nomanme <abdullah.noman083@gmail.com>
2021-09-05 00:58:26 +0600

    cookies

---
+++ 38-browser-API-and-methods/cookie.js
@@ -0,0 +1,13 @@
+// 38-5 (advanced) Cookies, dev tool application tab, get cookie value
+
+const getCookie = (name) => {
+	const cookie = document.cookie;
+	const allCookie = cookie.split(';');
+	const findCookie = allCookie.find((c) => c.includes(name));
+
+	if (findCookie) {
+		('country=US');
+		const cookieNameValue = findCookie.split('=');
+		return cookieNameValuep[1];
+	}
+};
