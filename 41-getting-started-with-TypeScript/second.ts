7e20dff8d2d108a57396b717ed40a563ba6f0bbc
nomanme <abdullah.noman083@gmail.com>
2021-09-10 02:06:27 +0600

    parameter & return type

--- 41-getting-started-with-TypeScript/second.ts
+++ 41-getting-started-with-TypeScript/second.ts
@@ -8,3 +8,27 @@
 let money: number = 5000;
 const myname: string = 'Mohammad Noman';
 const temperature: boolean = true;
+
+// 41-4 Set function parameter type and function return type
+
+const add = (first: number, second: number): number => {
+	const result = first + second;
+	return result;
+};
+
+const output: number = add(2, 3);
+
+console.log(output);
+
+// const addOne = (first: number | string, second: number | string): number | string => {
+// 	const result: number | string = first + second;
+// 	return result;
+// };
+
+// const fullName = add('Mohammad', 'Noman');
+
+// console.log(fullName);
+
+function doubleNumber(number: number): void {
+	console.log(2 * number);
+}
