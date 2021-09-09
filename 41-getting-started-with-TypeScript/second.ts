6c77ce8ea91bbb1df31173676e14c5293e12b4cc
nomanme <abdullah.noman083@gmail.com>
2021-09-10 01:11:59 +0600

    basic

---
+++ 41-getting-started-with-TypeScript/second.ts
@@ -0,0 +1,10 @@
+// 41-3 Basic typescript type number string boolean
+
+// let money = 5000;
+// money = '';
+// money = false;
+// money = 120;
+
+let money: number = 5000;
+const myname: string = 'Mohammad Noman';
+const temperature: boolean = true;
